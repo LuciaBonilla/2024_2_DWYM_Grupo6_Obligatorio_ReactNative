b918f5ca6462bac57646571467d2e2067cf31418
GerMarquez <germanmrqz@gmail.com>
2024-11-24 13:42:17 -0300

    Funcionalidad básica de OtherUserProfile adaptada desde React Web

---
+++ PhantyNet/app/components/OtherUserProfileScreen/ImagesContainer.tsx
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
+import BACKEND_URI from "@/constants/BACKEND_URI";
+import { useRouter } from "expo-router";
+import { Post } from "@/constants/types";
+import { useAuthContext } from "@/context-providers/AuthContextProvider";
+import { colors } from "@/constants/colors";
+
+type ImagesContainerProps = {
+  userAuthorPostsID: string;
+  posts: Post[];
+};
+
+const ImagesContainer: React.FC<ImagesContainerProps> = ({ userAuthorPostsID, posts }) => {
+  const [postsSorted, setPostsSorted] = useState<Post[]>(sortPosts(posts));
+  const router = useRouter();
+  const { userID } = useAuthContext();
+
+  function sortPosts(posts: Post[]): Post[] {
+    return posts.sort((post1, post2) => new Date(post2.createdAt).getTime() - new Date(post1.createdAt).getTime());
+  }
+
+  function handleGoToOtherUserPostPage(postID: string) {
+    //router.push(`/post/${postID}`);
+  }
+
+  function handleGoToMyPostPage(postID: string) {
+    //router.push(`/my-posts/${postID}`);
+  }
+
+  return (
+    <View style={styles.container}>
+      {postsSorted.length > 0 ? (
+        postsSorted.map((post) => (
+          <TouchableOpacity
+            key={post.id}  
+            onPress={() =>
+              userAuthorPostsID === userID
+                ? handleGoToMyPostPage(post.id)
+                : handleGoToOtherUserPostPage(post.id)
+            }
+            style={styles.imageCard}
+          >
+            <Image
+              style={styles.image}
+              source={{
+                uri: `${BACKEND_URI}/${post.imageUrl.replace("\\", "/")}`,
+              }}
+            />
+          </TouchableOpacity>
+        ))
+      ) : (
+        <Text style={styles.noPostsMessage}>No hay posts de este usuario</Text>
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    padding: 10,
+  },
+  imageCard: {
+    margin: 10,
+    width: 100,
+    height: 100,
+    borderRadius: 8,
+    overflow: "hidden",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover",
+  },
+  noPostsMessage: {
+    fontSize: 16,
+    color: colors.shadowColor,
+    textAlign: "center",
+  },
+});
+
+export default ImagesContainer;
--- PhantyNet/app/modules/(profiles)/OtherUserProfile/[id].tsx
+++ PhantyNet/app/modules/(profiles)/OtherUserProfile/[id].tsx
@@ -1,11 +1,96 @@
-import { Text, View } from "react-native";
+import { useLocalSearchParams } from "expo-router";
+import { Text, View, StyleSheet, Platform } from "react-native";
+import { useEffect, useState } from "react";
+import { UserInfo, Post, User } from "@/constants/types";
+
+
+// PROVEEDOR DE CONTEXTO.
+import { useAuthContext } from "@/context-providers/AuthContextProvider";
+import { useWindowDimensions } from "@/context-providers/WindowDimensionsProvider";
+
+// RUTAS.
+import routes from "@/constants/routes";
+
+// COLORES.
+import { colors } from "@/constants/colors";
+
+// ÍCONOS.
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+
+import BackendCaller from "@/auxiliar-classes/BackendCaller";
+import ProfileCard from "@/app/components/OtherUserProfileScreen/ProfileCard";
+import ImagesContainer from "@/app/components/OtherUserProfileScreen/ImagesContainer";
 
 export default function OtherUserProfile() {
+
+    const { width, height } = useWindowDimensions();
+    const [styles, setStyles] = useState(createStyles(width, height));
+
+    const { id } = useLocalSearchParams();
+
+    const { token } = useAuthContext();
+
+    const [userInfo, setUserInfo] = useState<UserInfo | null>(null);
+
+    useEffect(() => {
+        setStyles(createStyles(width, height))
+    }, [width, height]);
+
+    useEffect(() => {
+        async function fetchUserInfo() {
+          if (!id) return;
+          const response = await BackendCaller.getUserProfile(id, token);
+          if (response?.statusCode === 200) {
+            setUserInfo(response.data);
+            console.log(response.data);
+          }
+        }
+        fetchUserInfo();
+      }, [id, token]);
+
+    
     return (
         <View>
             <Text>
-                OTHER USER PROFILE
+                OTHER USER PROFILE - id = {id}
             </Text>
+
+            {userInfo ? (
+                <Text>Se encuentra usuario - {userInfo.user.username}</Text>
+            ):(
+                <Text>No Hay Usuario</Text>
+            )}
+            {userInfo?.user ? (
+            <ProfileCard user={userInfo?.user} postsQuantity={userInfo?.posts.length}/>
+            ):(
+                <Text>Something went wrong in card...</Text>
+            )}
+            {userInfo?.user ? (
+                <ImagesContainer
+                    userAuthorPostsID={userInfo.user.id}
+                    posts={userInfo.posts}
+                />
+            ) : (
+                <Text>Something went wrong in images...</Text>
+            ) 
+        };
         </View>
     )
+}
+
+function createStyles(width: number, height: number) {
+    return StyleSheet.create({
+        rootView: {
+            flex: 1,
+            backgroundColor: colors.background1Color,
+
+            justifyContent: "center",
+            alignItems: "center",
+
+            width,
+            height,
+
+            paddingTop: Platform.OS === "android" ? 20 : 0,
+        },
+    });
 }
